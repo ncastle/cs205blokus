d8c8f5e8bed8822c47a9b4124c56bc6bb7dd5e17
Nickolas.Castle <ncastle@uvm.edu>
2018-12-04 10:26:56 -0500

    Refactor

--- blokus/src/Blokus.java
+++ blokus/src/Blokus.java
@@ -1,15 +1,14 @@
-
 import javafx.animation.KeyFrame;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.util.Duration;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
+import javafx.scene.Group;
 import javafx.stage.Stage;
+
+import static javax.print.attribute.standard.Chromaticity.COLOR;
 
 /**
  * The blokus Application, which contains the board and a message label.
@@ -19,10 +18,13 @@ public class Blokus extends Application {
 
     public static double MILLISEC = 300;
     private Game game;
-    private Board board;
-    private Board inventory;
+    //private Board board;
+    //private Board inventory;
     private Timeline animation;
-    private Label statusLabel;
+
+
+    //Stage variable for current stage to display
+    private Stage primaryStage;
 
     /**
      * Launches the application.
@@ -30,52 +32,30 @@ public class Blokus extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
 
     /**
      * Sets up the blokus board and game, as well as a status label that can be
-     * used to display scores and messages.
-     *
+     * used to display scores and messages
      *
      * @param primaryStage
      * @throws Exception
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        board = new Board();
-        inventory = new Board(0.5);
+        this.primaryStage = primaryStage;
 
-        statusLabel = new Label("");
-        statusLabel.setTextFill(Color.BLACK);
+        //Scene scene = new Scene(layout);    //create scene object
 
-        BorderPane pane = new BorderPane();
-        pane.setCenter(board);
-        pane.setTop(statusLabel);
-        pane.setRight(inventory);
-
-        Scene scene = new Scene(pane);
-
-        game = new Game(this, board, inventory);
+        game = new Game(new Group(), this);
 
         // Can add events that occur automatically here.
         // setUpAnimation();
-        setUpKeyPresses();
 
-        setUpMouseEvents();
-
-        primaryStage.setScene(scene);
-
-        primaryStage.show();
-    }
-
-    /**
-     * Changes the message in the status label at the top of the screen.
-     *
-     * @param message
-     */
-    public void setMessage(String message) {
-        statusLabel.setText(message);
+        this.primaryStage.setTitle("Blokus");   //set title to stage
+        this.primaryStage.setScene(game);      //add scene to stage
+        this.primaryStage.show();              //display contents of stage
     }
 
     /**
@@ -94,55 +74,6 @@ public class Blokus extends Application {
 //        animation.setCycleCount(Timeline.INDEFINITE);
 //        animation.play();
 //    }
-    /**
-     * Sets up key events for the arrow keys and space bar. All keys send
-     * messages to the game, which should react appropriately.
-     */
-    private void setUpKeyPresses() {
-        board.setOnKeyPressed(e -> {
-            switch (e.getCode()) {
-                case LEFT:
-                    game.rotateLeft();
-                    break;
-                case RIGHT:
-                    game.rotateRight();
-                    break;
-                case DOWN:
-                    break;
-                case UP:
-                    game.mirror();
-                    break;
-                case M:
-                    game.mirror();
-                    break;
-                case SPACE:
-                    game.checkForMove();
-                    break;
-
-            }
-        });
-        board.requestFocus(); // board is focused to receive key input
-
-    }
-
-    /**
-     * Handles the mouse click.
-     */
-    private void setUpMouseEvents() {
-        board.setOnMousePressed(e -> {
-            game.placePiece();
-        });
-        board.setOnMouseMoved(e -> {
-            game.hover(e.getSceneX(), e.getSceneY());
-        });
-        //inventory.setOnMousePressed(e -> {
-        //    game.selectPiece();
-        //});
-        inventory.setOnMouseMoved(e -> {
-            game.hover(e.getSceneX(), e.getSceneY());
-        });
-        
-    }
 
     /**
      * Pauses the animation.
